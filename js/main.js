477f63a10546418def422282d381ae44a757a086
mtswaim <mtswaim@gmail.com>
2019-09-02 15:40:30 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen";
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardTwo);
+
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User Flipped Queen");
+var cardTwo = cards[2]
+cardsInPlay.push(cardTwo);
+console.log("User Flipped King");
+if (cardsInPlay.length===2) {
+	if(cardOne === cardTwo) {
+		alert("You found a match!");
+	} else {
+		alert ("Sorry, try again")
+	}
+}
+
